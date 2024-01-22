88fef07213104536b357384a0cf714713d32fd0a
Licar <84367661+Licarsmeth@users.noreply.github.com>
2024-01-22 13:54:35 +0545

    add asset resource. add icon.png and import it to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import myName from './myName';
 import _ from 'lodash';
 import './style.css';
+import Icon from './icon.png';
 
 function component() {
   const element = document.createElement('div');
@@ -9,6 +10,12 @@ function component() {
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+  
+  //Add the image to our existing div. 
+   const myIcon = new Image();
+   myIcon.src = Icon; 
+
+   element.appendChild(myIcon);
 
   //using myName function
   element.textContent = myName('Panda');
