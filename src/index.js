439f456ebcfb70b596798a2e7b6756787fac7043
Licar <84367661+Licarsmeth@users.noreply.github.com>
2024-01-22 18:21:39 +0545

    delete unnecessary items after testing them.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-import myName from './myName';
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
+
 
 function component() {
   const element = document.createElement('div');
@@ -9,16 +7,6 @@ function component() {
   // Lodash, currently included via a script, is required for this line to work (before adding lodash) 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  
-  //Add the image to our existing div. 
-   const myIcon = new Image();
-   myIcon.src = Icon; 
-
-   element.appendChild(myIcon);
-
-  //using myName function
-  element.textContent = myName('Panda');
 
   return element;
 }
